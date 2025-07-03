9a5f2b67aa5da63c3db634d880cd6db509ce2578
nick007116 <nick007116@gmail.com>
2025-07-03 13:23:19 +0530

    Committing the code

---
+++ even.java
@@ -0,0 +1,11 @@
+public class EvenNumberCheck {
+    public static void main(String[] args) {
+        int number = 10; // The number to check
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is not an even number (it's odd).");
+        }
+    }
+}
